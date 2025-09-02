a4d6dbbf762ef170dac86039e4024ef6dcfd6222
Matvey Zholudz <fili.kabban@gmail.com>
2025-09-02 19:04:45 +0300

    Add docs for ExceptionHandler

--- src/main/java/by/bonenaut7/uebus/ExceptionHandler.java
+++ src/main/java/by/bonenaut7/uebus/ExceptionHandler.java
@@ -1,5 +1,11 @@
 package by.bonenaut7.uebus;
 
 public interface ExceptionHandler {
+	
+	/**
+	 * Exception handler, should handle or re-throw supplied throwables.
+	 * 
+	 * @param throwable The throwable object
+	 */
 	void handle(Throwable throwable);
 }
